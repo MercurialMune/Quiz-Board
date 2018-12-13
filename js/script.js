661b99c87109982f5ffb221b626bfac3fba12c5d
MercurialMune <munenecyp@gmail.com>
2018-12-13 14:53:17 +0300

    correct js logic to fulfil objectives

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-$(document).ready(function(){
-$("").click(function(){
+$(document).ready(function() {
+  $(".showResults").hide();
 
+  $("#finsendit").click(function() {
+    $("#quizform").hide();
+    $(".showResults").toggle();
 
+    var options = [];
+    var right = 0;
 
+    options.push(parseInt($("input[name=1]:checked").val()));
+    options.push(parseInt($("input[name=2]:checked").val()));
+    options.push(parseInt($("input[name=3]:checked").val()));
+    options.push(parseInt($("input[name=4]:checked").val()));
+    options.push(parseInt($("input[name=5]:checked").val()));
+    options.push(parseInt($("input[name=6]:checked").val()));
+    options.push(parseInt($("input[name=7]:checked").val()));
+    options.push(parseInt($("input[name=8]:checked").val()));
+    options.push(parseInt($("input[name=9]:checked").val()));
+    options.push(parseInt($("input[name=10]:checked").val()));
 
+    for (var option in options) {
+      right += options[option];
+    }
 
+    $(".showResults").show();
 
-});
+    $(".showResults").html("Ssup! Out of a possible 100 , your score was" + " " + right);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 });
