a24b235a2fefc5d017263894dbca19693140cd10
MercurialMune <munenecyp@gmail.com>
2018-12-13 09:43:12 +0300

    nest onClick function within page load func()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-$(document).ready(function() {
-/* This would select all the elements */
-$("*").css("background-color", "yellow");
+$(document).ready(function(){
+$("").click(function(){
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
